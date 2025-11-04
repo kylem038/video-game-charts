7f1350c2cc957ae533de7683e251b78cd85ac54c
Kyle <misenckj@gmail.com>
2025-11-04 11:39:09 -0700

    initial commit

---
+++ app/app.tsx
@@ -0,0 +1,10 @@
+import Dashboard from "./components/dashboard";
+
+const App = () => (
+    <div className="flex flex-col">
+        <p>This is the start</p>
+        <Dashboard />
+    </div>
+);
+
+export default App;
